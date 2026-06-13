bcc34b6ef0db68009a5140664624054be978775b
Helder de Sousa Ramos <helder.sramos@gmail.com>
2026-06-13 09:52:36 -0300

    Iniciando estudos com inclusão de cards no anki

---
+++ Perkovic/Capitulo02/pp2_1.py
@@ -0,0 +1,38 @@
+'''Problema Prático 2.1
+Escreva expressões algébricas Python correspondentes aos seguintes comandos:
+(a)A soma dos 5 primeiros inteiros positivos.
+(b)A idade média de Sara (idade 23), Mark (idade 19) e Fátima (idade 31).
+(c)O número de vezes que 73 cabe em 403.
+(d)O resto de quando 403 é dividido por 73.
+(e)2 à 10a potência.
+(f)O valor absoluto da distância entre a altura de Sara (54 polegadas) e a altura de Mark
+(57 polegadas).
+(g)O menor preço entre os seguintes preços: R$ 34,99, R$ 29,95 e R$ 31,50.'''
+
+# (a)A soma dos 5 primeiros inteiros positivos.
+
+a = 1 + 2 + 3 + 4 + 5
+
+# (b)A idade média de Sara (idade 23), Mark (idade 19) e Fátima (idade 31).
+
+b = (23 + 19 + 31) / 3
+
+# (c)O número de vezes que 73 cabe em 403.
+
+c = 403 // 73
+
+# (d)O resto de quando 403 é dividido por 73.
+
+d = 403 % 73
+
+# (e)2 à 10a potência.
+
+e = 2**10
+
+# (f)O valor absoluto da distância entre a altura de Sara (54 polegadas) e a altura de Mark (57 polegadas).
+
+f = abs(54 - 57)
+
+# (g)O menor preço entre os seguintes preços: R$ 34,99, R$ 29,95 e R$ 31,50.
+
+g = min(34.99, 59.95, 31.50)
