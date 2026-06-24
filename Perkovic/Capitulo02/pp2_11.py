5d2e97804fb590e631305b7a9ccdbe188ddf62f1
Helder-Ramos <helder.sramos@gmail.com>
2026-06-24 12:08:59 -0300

    Término do pp2_11.py

--- Perkovic/Capitulo02/pp2_11.py
+++ Perkovic/Capitulo02/pp2_11.py
@@ -9,3 +9,28 @@ Deverá haver uma caneta de tartaruga no lado esquerdo da tela. Em seguida, exec
 sequência de comandos turtle graphics do Python que produzirão esta imagem:'''
 
 import turtle
+
+s = turtle.Screen()
+
+t = turtle.Turtle(shape='turtle')
+t.penup()
+t.goto(-300, 0)
+t.pendown()
+
+for i in range(9):
+    t.setheading(-45)
+    t.circle(50, 90)
+
+t.penup()
+t.goto(-100, 200)
+t.pendown()
+t.circle(50)
+t.penup()
+t.goto(0, -50)
+
+t.penup()
+t.goto(0, -50)
+
+t.right(20)
+
+s.mainloop()
--- Perkovic/Capitulo02/pp2_5.py
+++ Perkovic/Capitulo02/pp2_5.py
@@ -11,7 +11,7 @@ Agora, escreva expressões usando a string s e o operador de indexação que é 
 s = '0123456789'
 
 a = s[0]
-b = [1]
-c = [6]
-d = [8]
-e = [9]
+b = s[1]
+c = s[6]
+d = s[8]
+e = s[9]
