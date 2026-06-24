45344f0f8a33f784a4ee0a4faa4ee1f80c51ccf3
Helder de Sousa Ramos <helder.sramos@gmail.com>
2026-06-24 10:47:35 -0300

    iníciado pp2_11.py

---
+++ Perkovic/Capitulo02/pp2_11.py
@@ -0,0 +1,11 @@
+'''Problema Prático 2.11
+Comece executando estas instruções:
+>>> s = turtle.Screen()
+>>> t = turtle.Turtle(shape='turtle')
+>>> t.penup()
+>>> t.goto(-300, 0)
+>>> t.pendown()
+Deverá haver uma caneta de tartaruga no lado esquerdo da tela. Em seguida, execute uma
+sequência de comandos turtle graphics do Python que produzirão esta imagem:'''
+
+import turtle
