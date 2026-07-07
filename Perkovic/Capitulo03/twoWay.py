594a599bf4209db9c485c161d1e85bd0f2fc0bf5
Helder-Ramos <helder.sramos@gmail.com>
2026-07-07 11:57:20 -0300

    Realizado até exercício twoWay.py

---
+++ Perkovic/Capitulo03/twoWay.py
@@ -0,0 +1,13 @@
+temp = eval(input('Digite a temperatura atual: '))
+
+if temp > 86:
+
+    print('Está quente!')
+    print('Beba bastante líquido.')
+
+else:
+    
+    print('Não está quente.')
+    print('Traga uma jaqueta.')
+
+print('Adeus')
